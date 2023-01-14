1b32f9d703d8b11c341a75f40b6e8abaa003ea97
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2023-01-14 14:37:31 -0300

    Update Administrador.cs

--- Models/Administrador.cs
+++ Models/Administrador.cs
@@ -1,5 +1,8 @@
+using System.ComponentModel.DataAnnotations.Schema; 
+    
 namespace api.Models;
 
+[Table("administradores")]
 public record Administrador
 {
     public int Id { get;set; } = default!;
